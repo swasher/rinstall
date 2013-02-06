f8fad2b1a293db0d59d65a39235c0ae7c70d4968
Swasher <mr.swasher@gmail.com>
2013-02-06 16:38:30 +0200

    commit

--- rinstall.sh
+++ rinstall.sh
@@ -10,6 +10,10 @@ PASS = pass
 USERWEB = swasher
 PASSWEB = swasher
 
+#Choose web server
+#WEBSERVER = apache
+WEBSERVER = lighttpd
+
 
 #### END SETUP ####
 
@@ -24,11 +28,75 @@ useradd $USER -p $PASS
 # возвращает код ошибки от useradd
 
 cd /home
-mkdir rtorrent
+mkdir $USER
 cd rtorrent
 mkdir torrents
 mkdir session
 
-chown -R rtorrent /home/rtorrent/
-chgrp -R rtorrent /home/rtorrent/
+wget https://raw.github.com/swasher/rinstall/master/rtorrent.rc -O .rtorrent.rc
+
+chmod 666 .rtorrent.rc
+chown -R rtorrent /home/$USER/.rtorrent.rc
+chgrp -R rtorrent /home/$USER/.rtorrent.rc
+
+####
+#
+
+case $WEBSERVER in
+    lighttpd)
+	apt-get install lighttpd
+
+	cat >> /etc/lighttpd/conf-available/10-fastcgi.conf <<End-of-message
+	fastcgi.server = ( ".php" =>
+	    ((
+		"bin-path" => "/usr/bin/php5-cgi",
+		"socket" => "/tmp/php.socket",
+		"max-procs" => 2,
+		"idle-timeout" => 20,
+		"bin-environment" => (
+		"PHP_FCGI_CHILDREN" => "1",
+		"PHP_FCGI_MAX_REQUESTS" => "10000"
+		),
+		"bin-copy-environment" => (
+		"PATH", "SHELL", "USER"
+	         ),
+		"broken-scriptfilename" => "enable"
+	     ))
+	)
+	End-of-message
+
+	lighttpd-enable-mod auth
+	
+	cat >> /etc/lighttpd/conf-available/05-auth.conf <<End-of-message
+	auth.backend                   = "htdigest"
+	auth.backend.htdigest.userfile = "/etc/lighttpd/htdigest"
+
+	auth.require = ( "/RPC2" =>
+	    (
+            "method" => "digest",
+            "realm" => "rTorrent RPC",
+            "require" => "user=rtorrent"
+            )
+	)
+	End-of-message
+
+	;;
+    
+    apache)
+	#install apache
+	;;
+
+
+#Создаем пароль, который будет спрашиваться при доступе через веб-интерфейс:
+# Руками использоваласт команда
+# htdigest -c /etc/lighttpd/htdigest "rTorrent RPC" rtorrent
+# Скрипт пишет файл напрямую, потому что htdigest спрашивает пароль из терминала
+# htdigest так же зависит от apache2-utils
+
+cd /etc/lighttpd
+hash=`echo -n "$USERWEB:RPC:$PASSWEB" | md5sum | cut -b -32`
+echo "$user:$realm:$hash" > /etc/lighttpd/htdigest
+
+
+
 
