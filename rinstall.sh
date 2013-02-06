e68a732d7b6babd88c0ce49cd875c3a4e5971dcb
Swasher <mr.swasher@gmail.com>
2013-02-06 13:59:53 +0200

    1

--- rinstall.sh
+++ rinstall.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 
-ff
+#### SETUP SECTION  ####
+
+#user and password for rtorrent process
+USER = rtorrent
+PASS = pass
+
+#login and password for access rutorrent (user NOT created)
+USERWEB = swasher
+PASSWEB = swasher
+
+
+#### END SETUP ####
+
+
+apt-get update -y && apt-get upgrade -y
+apt-get install -y subversion php5-cgi screen apache2-utils php5-cli
+apt-get install -y rtorrent
+
+useradd $USER -p $PASS
+
+# echo $?
+# возвращает код ошибки от useradd
+
+cd /home
+mkdir rtorrent
+cd rtorrent
+mkdir torrents
+mkdir session
+
+chown -R rtorrent /home/rtorrent/
+chgrp -R rtorrent /home/rtorrent/
+
